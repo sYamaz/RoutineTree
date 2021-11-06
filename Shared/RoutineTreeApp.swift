9fe1eadfce52e5577bd68b27eafc54d2f9add6ea
Shun Yamazaki <syunn820@gmail.com>
2021-11-06 20:06:21 +0900

    Initial Commit

---
+++ Shared/RoutineTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutineTreeApp.swift
+//  Shared
+//
+//  Created by Shun Yamazaki on 2021/11/06.
+//
+
+import SwiftUI
+
+@main
+struct RoutineTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
